7798005f736238c2165194fb3ab271120c45bd4b
Laxmi Amruta Padhi <164632284+laxmiamrutapadhi@users.noreply.github.com>
2025-10-25 17:16:21 +0530

    Add HTML to Tailwind content paths and extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: { DEFAULT: '200ms' },
+    },
   },
   plugins: [],
-} 
+}
